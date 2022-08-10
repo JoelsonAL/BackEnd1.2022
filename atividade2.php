2e6bb6234581cf4738eb0acc9d38f36618a0a961
JoelsonAL <110984742+JoelsonAL@users.noreply.github.com>
2022-08-10 15:19:26 -0400

    Add files via upload

---
+++ atividade2.php
@@ -0,0 +1,10 @@
+ <?php
+
+ $linguagens = array("PHP", "DELPHI", "JAVA", "ASP", "JAVASCRIPT", "RUBBY", "C#");
+ sort($linguagens);
+ foreach ($linguagens as $linguagem) {
+
+   echo "$linguagem<br>";
+ }
+  echo "<br>";
+ ?>
